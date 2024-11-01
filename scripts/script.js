05d801521d7d74475ad5f8b1871520f417dfc31d
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-11-01 17:52:21 +1000

    Handleing search. Not rendering to page yet.
    Clumsy workaround as the omdbapi search parameter does not have all the data required for rendering the results required.
    
    Essentially, the search parameter gets the (for our purposes) title. Then researches using the 'title' parameter and returns an array with each index being a movie.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,42 @@
 // console.log('I\'m working')
+const mainEl = document.getElementById('main');
+const searchResultsSection = document.getElementById('search-results-section');
+const formEl = document.getElementById('search-form');
+
+document.addEventListener('click', async (e) => {
+    //console.log('hi')
+    if (e.target.value === 'submit'){
+        e.preventDefault();
+        handleSearch();
+    }   
+
+})
+
+// Clumsy workaround as the omdbapi searh does not have all the data required for 
+// rendering the results required
+
+async function handleSearch () {
+    const forms = new FormData(formEl)
+    const searchQuery = forms.get('search-query')
+
+    const response = await fetch(`http://www.omdbapi.com/?s=${searchQuery}&plot=full&apikey=3bcc61`, {
+        method: "GET",
+    })
+     const data = await response.json();
+    let movieArr =  []
+     for (let i = 0; i < 3; i++){
+        movieArr.push(data.Search[i].Title)
+     }
+     const searchResults = await Promise.all(
+        movieArr.map(async (movieTitle) => {
+            const response = await fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=3bcc61`);
+            const data = await response.json();
+            return data;
+        })
+    );
+    console.log(searchResults);
+    return searchResults;
+     
+    //  console.log(movieArr)
+
+}
