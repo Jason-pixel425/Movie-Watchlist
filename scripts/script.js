84dca072431e94165d94a7d346b4949e5b3cf789
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-11-02 16:41:04 +1000

    added a renderSearch function (Working)...
    ... It still needs styling.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // console.log('I\'m working')
 //Testing data
 import { dummyDataArr } from "./dummyData.js";
+console.log(dummyDataArr)
 
 const mainEl = document.getElementById('main');
 const searchResultsSection = document.getElementById('search-results-section');
@@ -23,7 +24,7 @@ async function handleSearch () {
     const searchQuery = forms.get('search-query')
     const numOfResults = Number(forms.get('num-of-results')) ?  Number(forms.get('num-of-results')) : 1;
 
-    const response = await fetch(`http://www.omdbapi.com/?s=${searchQuery}&plot=full&apikey=3bcc61`, {
+    const response = await fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=3bcc61`, {
         method: "GET",
     })
      const data = await response.json();
@@ -47,3 +48,35 @@ async function handleSearch () {
     //  console.log(movieArr)
 
 }
+
+function renderSearch(movieArr) {
+    let renderString = ''
+    renderString += movieArr.map(movie => {
+        return `
+            <img src="${movie.Poster}" />
+            <div class="movie-container">
+
+                <div class="movie-top-container">
+                    <h2>${movie.Title}</h2>
+                    <p>⭐</p>
+                    <p>${movie.imdbRating}</p>
+                </div>
+                    
+                <div class="movie-middle-container">
+                    <p>${movie.Runtime}</p>
+                    <p>${movie.Genre}</p>
+                    <button><img src="../styles/images/add-icon.png" /> Watchlist</button>
+                </div>
+
+                <div class="movie-bottom-container">
+                    <p>${movie.Plot}</p>
+                </div>
+                
+            </div>
+            
+            
+        `
+    }).join('')
+    searchResultsSection.innerHTML = renderString
+}
+renderSearch(dummyDataArr)
