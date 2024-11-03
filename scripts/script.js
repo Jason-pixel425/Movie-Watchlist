c295b8c90975c8cdc755d653c7f1993de41f71ac
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-11-03 18:29:58 +1000

    add and removing items from local storage working
    Now just need to handle empty watchlist
    and need to handle no search results

--- scripts/script.js
+++ scripts/script.js
@@ -6,20 +6,42 @@ console.log(dummyDataArr)
 const mainEl = document.getElementById('main');
 const searchResultsSection = document.getElementById('search-results-section');
 const formEl = document.getElementById('search-form');
+// Determine path for add/remove button text
+const path = window.location.pathname
+// Local storage savedMovieArr
+let savedMovieArr = JSON.parse(window.localStorage.getItem('movies')) || [];
+
+
+if (path.includes('Watchlist.html')){
+    renderWatchlist(); 
+ }
+ renderSearch(dummyDataArr)
+
+
 
 document.addEventListener('click', async (e) => {
-    //console.log('hi')
     if (e.target.value === 'submit'){
         e.preventDefault();
         mainEl.classList.remove('toggle-background')
         handleSearch();
-    }   
+    } else if (path.includes('index.html')){
+        if (e.target.dataset && e.target.dataset.imdbid) {
+            if(!savedMovieArr.includes(e.target.dataset.imdbid))
+            savedMovieArr.push(e.target.dataset.imdbid);
+            saveArrayToLocalStorage("movies", savedMovieArr);
+        }
+    } else if (path.includes('Watchlist.html')) {
+        if (e.target.dataset && e.target.dataset.imdbid) {
+            savedMovieArr = savedMovieArr.filter(imdbid => imdbid != e.target.dataset.imdbid);
+            saveArrayToLocalStorage("movies", savedMovieArr)
+            renderWatchlist()
+        }
+    }
 
 })
 
 // Clumsy workaround as the omdbapi searh does not have all the data required for 
 // rendering the results required
-
 async function handleSearch () {
     const forms = new FormData(formEl)
     const searchQuery = forms.get('search-query')
@@ -47,7 +69,24 @@ async function handleSearch () {
     renderSearch(searchResults);
 }
 
+//Dealing with local Storage
+function getArrayFromLocalStorage(arrayName){
+ return JSON.parse(window.localStorage.getItem(arrayName));
+}
+
+function saveArrayToLocalStorage(arrayName, array) {
+    window.localStorage.setItem(arrayName, JSON.stringify(array));
+  }
+
+//render user search
 function renderSearch(movieArr) {
+    let buttonText = ''
+    if (path.endsWith('index.html')){
+        buttonText = '<img src="../styles/images/add-icon.png" /> Watchlist';
+    } else {
+        buttonText = '<img src="../styles/images/remove-icon.png" /> Remove';
+    }
+
     let renderString = ''
     renderString += movieArr.map(movie => {
         return `
@@ -64,7 +103,7 @@ function renderSearch(movieArr) {
                 <div class="movie-middle-container">
                     <p>${movie.Runtime}</p>
                     <p>${movie.Genre}</p>
-                    <button class="add-btn" data-imdbID="${movie.imdbId}"><img src="../styles/images/add-icon.png" /> Watchlist</button>
+                    <button class="add-btn" data-imdbid="${movie.imdbID}">${buttonText}</button>
                 </div>
 
                 <div class="movie-bottom-container">
@@ -76,4 +115,21 @@ function renderSearch(movieArr) {
     }).join('')
     searchResultsSection.innerHTML = renderString
 }
-renderSearch(dummyDataArr)
+
+//Render user watchlist
+async function renderWatchlist() {
+    let savedMovieArr = getArrayFromLocalStorage('movies') || [];
+    if (savedMovieArr.length === 0) {
+        searchResultsSection.innerHTML = '<p>Your watchlist is empty.</p>';
+    } else {
+        const movieDataArr = await Promise.all(
+            savedMovieArr.map(async (imdbID) => {
+                const response = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=3bcc61`);
+                const data = await response.json();
+                return data;
+            })
+        );
+        renderSearch(movieDataArr);
+    }
+}
+
